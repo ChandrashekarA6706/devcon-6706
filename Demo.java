16b07e47767f5aba199eea713a0845a9b65fe72b
ChandrashekarA6706 <23211a6706@bvrit.ac.in>
2025-07-21 22:06:55 +0530

    Update Demo.java

--- Demo.java
+++ Demo.java
@@ -1,5 +1,5 @@
 public class Demo {
     public static void main(String[] args) {
-        System.out.println("Hello from Jenkins build!");
+        System.out.println("Hello this is edited file");
     }
 }
